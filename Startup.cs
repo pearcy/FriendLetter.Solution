b5dbc18f986f60df944ffa56407b76d4dfd81c99
Joseph Pearce <joseph.pearce05@gmail.com>
2020-07-19 18:26:14 -0700

    add Models, Views

--- Startup.cs
+++ Startup.cs
@@ -34,7 +34,7 @@ namespace FriendLetter
       });
       app.Run(async (context) =>
       {
-          await context.Response.WriteAsync("Hello World!");
+          await context.Response.WriteAsync("Hello World! I am watching");
       });
 
     }
